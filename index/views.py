f4864b25e2ace5ab5d48d5ca562408e353dae605
Kamich-GP <you@example.com>
2025-12-23 21:02:00 +0500

    urls, views

--- index/views.py
+++ index/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import Product, Category, Cart
 
 # Create your views here.
+# Главная страница
+def home_page(request):
+    # Достаем данные из БД
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    # Передаем данные на фронт
+    context = {
+        'products': products,
+        'categories': categories
+    }
+    return render(request, 'home.html', context)
+
+# Страница с категорией
+def category_page(request, pk):
+    # Достаем данные из БД
+    category = Category.objects.get(id=pk)
+    products = Product.objects.filter(product_category=category)
+    # Передаем данные на фронт
+    context = {
+        'category': category,
+        'products': products
+    }
+    return render(request, 'category.html', context)
+
+# Страница с товаром
+def product_page(request, pk):
+    # Достаем данные из БД
+    product = Product.objects.get(id=pk)
+    # Передаем данные на фронт
+    context = {'product': product}
+    return render(request, 'product.html', context)
+    
