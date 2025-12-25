3ac2911ad0dad3bebcc9fb68492a54005f4aed9a
Kamich-GP <yurochkagameplus@gmail.com>
2025-12-25 21:17:15 +0500

    templates

--- index/views.py
+++ index/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from .models import Product, Category, Cart
+import random
+
+messi_images = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe82d1P7W7j0Dgnx0bVmy-ICZ_HAKKkqiYiQ&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHRVNuogny_0nPUImPpRZd0Za9UydMvgfvkw&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjFkwsHvp6ICnlBedLNCIqlnS1-dKQ_qQdBA&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQcMTKnSzsLthPuZp0JQY0Hb4m0tilABZ-w&s',
+    'https://www.anhnghethuatdulich.com/wp-content/uploads/2025/08/messi-meme-face-khac-hoa-chan-thuc-bieu-cam-kho-quen-cua-sieu-sao-argentina.jpg',
+    'https://i.pinimg.com/originals/12/98/0f/12980f9b45cf3660e848ca2b70f29621.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWjkYQqGOk7al4vzW5g9FpPizmggJa9hhpXg&s',
+    'https://tomauchotre.com/wp-content/uploads/2025/10/messi-meme-22.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPr9Iz0_QNBBkrbDCL41GEjjbOvhuNcqoNPQ&s'
+]
 
 # Create your views here.
 # Главная страница
@@ -10,7 +23,8 @@ def home_page(request):
     # Передаем данные на фронт
     context = {
         'products': products,
-        'categories': categories
+        'categories': categories,
+        'image': random.choice(messi_images)
     }
     return render(request, 'home.html', context)
 
@@ -22,7 +36,8 @@ def category_page(request, pk):
     # Передаем данные на фронт
     context = {
         'category': category,
-        'products': products
+        'products': products,
+        'image': random.choice(messi_images)
     }
     return render(request, 'category.html', context)
 
@@ -31,6 +46,24 @@ def product_page(request, pk):
     # Достаем данные из БД
     product = Product.objects.get(id=pk)
     # Передаем данные на фронт
-    context = {'product': product}
+    context = {
+        'product': product,
+        'image': random.choice(messi_images)
+    }
     return render(request, 'product.html', context)
+
+# Поиск товара по названию
+def search(request):
+    if request.method == 'POST':
+        # Достаем данные с формы
+        searched_product = request.POST.get('search-product')
+        # Достаем данные из БД
+        get_product = Product.objects.filter(product_name__iregex=searched_product)
+        # Передаем данные на фронт
+        context = {}
+        if get_product:
+            context.update(user_pr=searched_product, products=get_product, image=random.choice(messi_images))
+        else:
+            context.update(user_pr=searched_product, products='', image=random.choice(messi_images))
+        return render(request, 'result.html', context)
     
